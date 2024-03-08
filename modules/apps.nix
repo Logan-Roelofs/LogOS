dab5f9bdb060c4aa326a4978a41d5927e980fde2
Logan-Roelofs <loroe333@gmail.com>
2024-03-08 09:26:08 -0600

    add inetutils

--- modules/apps.nix
+++ modules/apps.nix
@@ -19,6 +19,7 @@
     kitty           # The CLI itself
     gparted         # Partition manager
     ripgrep         # Fast file search   
+    inetutils       # ping, ifconfig, etc
 
     # Emulators and Game Managment
     steam-rom-manager # Steam ROM Manager
