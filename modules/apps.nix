d9208c670d7db83ed5799f5de1cf35a09af912dd
Logan-Roelofs <loroe333@gmail.com>
2024-03-08 18:48:14 -0600

    add ventoy-full

--- modules/apps.nix
+++ modules/apps.nix
@@ -19,6 +19,7 @@
     kitty           # The CLI itself
     gparted         # Partition manager
     ripgrep         # Fast file search   
+    ventoy-full     # USB bootable drive creator
 
     # Emulators and Game Managment
     steam-rom-manager # Steam ROM Manager
