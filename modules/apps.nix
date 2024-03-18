11fd4b532f7a493d3f8d561e401a5ffc0e3f21f9
Logan Roelofs <loroe333@gmail.com>
2024-03-18 11:24:54 -0500

    xfreerdp

--- modules/apps.nix
+++ modules/apps.nix
@@ -19,6 +19,7 @@
     kitty           # The CLI itself
     gparted         # Partition manager
     ripgrep         # Fast file search   
+    xfreerdp
 
     # Emulators and Game Managment
     steam-rom-manager # Steam ROM Manager
