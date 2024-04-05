22892115a3a266e7e60283572c02865d05307625
Logan Roelofs <loroe333@gmail.com>
2024-04-05 11:26:27 -0500

    rofi

--- modules/apps.nix
+++ modules/apps.nix
@@ -67,7 +67,7 @@
     slack           # Team communication
     teams-for-linux # Unifficial Microsoft Teams client
     discord         # Server communication
-    flameshot
+    rofi
 
     #looking-glass-client # GPU accelerated KVM
     #obs-studio-plugins.looking-glass-obs # Looking Glass OBS plugin
