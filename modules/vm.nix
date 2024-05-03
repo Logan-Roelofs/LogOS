41cb736d24c583a95f0dd78e23ab325b46c4672e
Logan Roelofs <loroe333@gmail.com>
2024-05-03 14:11:50 -0500

    vm

--- modules/vm.nix
+++ modules/vm.nix
@@ -6,7 +6,7 @@
   programs.dconf.enable = true;
 
   # Add user to libvirtd group
-  users.users.$user.extraGroups = [ "libvirtd" ];
+  users.users.logan.extraGroups = [ "libvirtd" ];
 
   # Install necessary packages
   environment.systemPackages = with pkgs; [
