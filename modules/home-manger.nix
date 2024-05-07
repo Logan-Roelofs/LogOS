68efd0f8b1aa41a1d2deabd2e702f0aa61b6f841
Logan Roelofs <loroe333@gmail.com>
2024-05-07 18:05:24 -0500

    add home-manger

---
+++ modules/home-manger.nix
@@ -0,0 +1,14 @@
+# How we set up home-manger independently of nixos
+# nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+#
+# nix-channel --update
+#
+# nix-shell '<home-manager>' -A install
+#
+# home-manager switch
+
+nix-channel --update
+{ config, pkgs, inputs, ... }:
+{
+  nix.settings.allowed-users = [ "*" ]; # Allow all users to use nix daemon (used for home-manager)
+}
