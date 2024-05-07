ed89b6453b1a70d47b0be27cf48fe1bbbe5ff501
Logan Roelofs <loroe333@gmail.com>
2024-05-07 18:07:53 -0500

    add home-manger

--- modules/home-manger.nix
+++ modules/home-manger.nix
@@ -7,7 +7,6 @@
 #
 # home-manager switch
 
-nix-channel --update
 { config, pkgs, inputs, ... }:
 {
   nix.settings.allowed-users = [ "*" ]; # Allow all users to use nix daemon (used for home-manager)
