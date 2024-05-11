11e0732d490a9281835bd5e8247970d131f80183
Logan Roelofs <loroe333@gmail.com>
2024-05-11 12:53:13 -0500

    add home-manger

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     ags.url = "github:Aylur/ags";
     #hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-    hyprland.url = "https://github.com/hyprwm/Hyprland/releases/tag/v0.40.0";
+    hyprland.url = pkgs.unstable.hyprland;
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
