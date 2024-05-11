37c725ff336de8f43052f1618294f134032d89a6
Logan Roelofs <loroe333@gmail.com>
2024-05-11 11:35:48 -0500

    add home-manger

--- modules/default.nix
+++ modules/default.nix
@@ -35,7 +35,6 @@
       enable = true;
       xwayland.enable = true;
       portalPackage = pkgs.xdg-desktop-portal-hyprland;
-      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
 
   # Define Common System Fonts
