ad3ab032d2de301c0d77691e92b852851ccbf1e2
Logan Roelofs <loroe333@gmail.com>
2024-05-11 14:29:39 -0500

    change hyprwayland-scanner version requierment

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -4,7 +4,7 @@
     enable = true;
     xwayland.enable = true;
     plugins = [
-      inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
+#      inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
     ];
     settings = { 
       
