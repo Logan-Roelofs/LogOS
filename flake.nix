f059c263777977af3f63f276ec942ebafee75bbd
Logan Roelofs <loroe333@gmail.com>
2024-05-11 12:56:36 -0500

    add home-manger

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     #hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "nixpkgs";
+      inputs.hyprland.follows = "hyprland";
     };
   };
 
