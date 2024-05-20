19506790b2f6da18b56d30639f4b1d549151c2a3
Logan Roelofs <loroe333@gmail.com>
2024-05-20 12:12:06 -0500

    add dark nord theme

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -28,7 +28,7 @@
         "shadow_range" = "4";
         "shadow_render_power" = "3";
         "drop_shadow" = "true";
-        "col.shadow" = "rgba(1a1a1aee)";
+#        "col.shadow" = "rgba(1a1a1aee)";
       };
 
       animations = {
