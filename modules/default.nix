015687315a8d6ad3adf8ef7203a4f85985bf99e0
Logan Roelofs <loroe333@gmail.com>
2024-05-20 11:00:52 -0500

    stable hyprland

--- modules/default.nix
+++ modules/default.nix
@@ -35,7 +35,6 @@
   programs.hyprland = {
       enable = true;
       xwayland.enable = true;
-      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
