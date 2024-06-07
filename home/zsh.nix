e8e58434fcf3fb69ad5735e59c44b8c61d5ee2b2
Logan Roelofs <loroe333@gmail.com>
2024-06-07 11:41:06 -0500

    zsh config

--- home/zsh.nix
+++ home/zsh.nix
@@ -11,21 +11,11 @@
       theme = "my_awesome_theme";
       plugins = [
         "git"
-      	"jovial"
       ];
     };
   };
-  home.file.".oh-my-zsh/custom/themes/my_awesome_theme.zsh-theme".text = ''
-  PROMPT='%m %B%F{blue}:: %B%F{blue}blue               %b%F{green}%3~ $(hg_prompt_info)$(git_prompt_info)%B%(!.%F{red}.%F{blue})»%f%b '
-  RPS1='%(?..%F{red}%? ↵%f)'
-
-  ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[yellow]%}‹"
-  ZSH_THEME_GIT_PROMPT_SUFFIX="› %{$reset_color%}"
-  
-  ZSH_THEME_HG_PROMPT_PREFIX="%{$fg[magenta]%}hg:‹%{$fg[yellow]%}"
-  ZSH_THEME_HG_PROMPT_SUFFIX="%{$fg[magenta]%}› %{$reset_color%}"
-  ZSH_THEME_HG_PROMPT_DIRTY=" %{$fg[red]%}✗"
-  ZSH_THEME_HG_PROMPT_CLEAN=""
-  '';
- 
+  home.file.".oh-my-zsh/custom/themes/my_awesome_theme.zsh-theme" = { 
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/logos/dotfiles/oh-my-zsh/";
+    recursive = true;
+  };
 }
