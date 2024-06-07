e6886c8eb97a9cbffa707b5520590566c3682c4b
Logan Roelofs <loroe333@gmail.com>
2024-06-06 19:22:28 -0500

    flavours

--- home/apps.nix
+++ home/apps.nix
@@ -10,5 +10,17 @@
   # Text Editor
   programs.vscode.enable = true;  
   programs.neovim.enable = true;
-
+  programs.neovim.defaultEditor = true;
+  programs.neovim.plugins = with pkgs.vimPlugins; [
+    telescope-nvim
+      plenary-nvim
+      # Also nvim-web-devicons
+      nui-nvim
+      image-nvim
+    neo-tree-nvim
+    barbar-nvim
+      nvim-web-devicons
+      gitsigns-nvim
+    copilot-vim
+  ];
 }
