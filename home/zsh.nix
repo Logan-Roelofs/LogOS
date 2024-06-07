06f402202fd9ce082a95744a432089f78cfeb76f
Logan Roelofs <loroe333@gmail.com>
2024-06-07 11:46:11 -0500

    zsh config

--- home/zsh.nix
+++ home/zsh.nix
@@ -8,13 +8,13 @@
       custom = "$HOME/.oh-my-zsh/custom";
       enable = true;
       # Theme is the prompt layout
-      theme = "my_awesome_theme";
+      theme = "logan";
       plugins = [
         "git"
       ];
     };
   };
-  home.file.".oh-my-zsh/custom/themes/my_awesome_theme.zsh-theme" = { 
+  home.file.".oh-my-zsh/custom/themes/logan.zsh-theme" = { 
     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/logos/dotfiles/oh-my-zsh/";
     recursive = true;
   };
