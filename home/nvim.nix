9fa329ac7f2211f176f96ddb27a7a66f3ec84969
Logan Roelofs <loroe333@gmail.com>
2024-06-07 15:06:12 -0500

    zsh config

--- home/nvim.nix
+++ home/nvim.nix
@@ -9,7 +9,7 @@
     
     # Programs
     telescope-nvim
-    neo-tree-nvim
+    nvim-tree-lua
       nui-nvim
       image-nvim
     barbar-nvim
@@ -34,9 +34,6 @@
   vim.api.nvim_set_keymap('v', '<C-v>', '"+p', { noremap = true, silent = true })
   vim.api.nvim_set_keymap('i', '<C-v>', '<Esc>"+pa', { noremap = true, silent = true })
   
-  -- File Tree
-  vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-  vim.keymap.set("n", "<C-m>", ":Neotree filesystem close left<CR>", {})
   
   -- BarBar
   vim.keymap.set('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
@@ -51,12 +48,80 @@
   vim.keymap.set('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
   vim.keymap.set('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
   vim.keymap.set('n', '<A-n>', function()
-    vim.cmd('enew')  -- Open a new buffer
-    vim.cmd('Neotree') -- Open Neo-tree in the new buffer
+    vim.cmd('enew')
+    vim.cmd('NvimTreeToggle')
   end, { noremap = true, silent = true })
-
+  
   -- Telescope
   vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>", {})
   vim.keymap.set("n", "<C-g>", ":Telescope live_grep<CR>", {})
+
+-- nvim-tree setup
+local setup, nvimtree = pcall(require, "nvim-tree")
+if not setup then return end
+
+vim.cmd([[
+  nnoremap - :NvimTreeToggle<CR>
+]])
+
+-- Uncomment the following lines if you want to use these key mappings and settings
+-- local keymap = vim.keymap -- for conciseness
+-- keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
+  vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>")
+
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- vim.opt.foldenable = false -- Disable folding at startup.
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
+local HEIGHT_RATIO = 0.8 -- You can change this
+local WIDTH_RATIO = 0.5  -- You can change this too
+
+nvimtree.setup({
+  disable_netrw = true,
+  hijack_netrw = true,
+  respect_buf_cwd = true,
+  sync_root_with_cwd = true,
+  view = {
+    relativenumber = true,
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * WIDTH_RATIO
+        local window_h = screen_h * HEIGHT_RATIO
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                         - vim.opt.cmdheight:get()
+        return {
+          border = "rounded",
+          relative = "editor",
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+      end,
+    },
+    width = function()
+      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+    end,
+  },
+  -- filters = {
+  --   custom = { "^.git$" },
+  -- },
+  -- renderer = {
+  --   indent_width = 1,
+  -- },
+})
+
+
   '';
 }
