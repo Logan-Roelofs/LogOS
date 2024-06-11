66ef925b14c8b08397c52748c01587d11a460e4b
Logan Roelofs <loroe333@gmail.com>
2024-06-11 16:40:49 -0500

    hyprland

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     ags.url = "github:Aylur/ags";
-    hyprland = { url = "github:hyprwm/Hyprland/tags/v0.40.0"; inputs.nixpkgs.follows = "nixpkgs"; };
+    hyprland = { url = "github:hyprwm/Hyprland"; inputs.nixpkgs.follows = "nixpkgs"; };
     hyprland-plugins = { url = "github:hyprwm/hyprland-plugins"; inputs.hyprland.follows = "hyprland"; };
     stylix.url = "github:danth/stylix";
   };
