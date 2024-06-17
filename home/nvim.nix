db2bcc85fa3e4ca72ae11f694fa00628c8d7de50
Logan Roelofs <loroe333@gmail.com>
2024-06-17 11:02:57 -0500

    nvim lsp

--- home/nvim.nix
+++ home/nvim.nix
@@ -126,7 +126,7 @@
       -- LSPconfig (Error Checking)
       --require'lspconfig'.quick_lint_js.setup{}
       require'lspconfig'.tsserver.setup{
-        cmd = { "javascript-typescript-stdio"},
+        cmd = {"javascript-typescript-langserver"},
       }
       require'lspconfig'.pyright.setup{}
 
