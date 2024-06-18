5cb89f23c7876dfd66c86610cd435135acf03004
Logan Roelofs <loroe333@gmail.com>
2024-06-18 12:54:04 -0500

    hyprlock

--- home/hyprlock.nix
+++ home/hyprlock.nix
@@ -1,11 +1,18 @@
 { inputs, config, pkgs, ... }: {
   programs.hyprlock.enable = true;
+
+  home.file.".config/hyprlock" = {
+    source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/.config/logos/dotfiles/hyprlock/";
+    recursive = true;
+  };
+
   programs.hyprlock.settings = {
     general = {
-      disable_loading_bar = false;
+      disable_loading_bar = true;
       grace = 1;
-      ignore_empty_input = true
-      text_trim = true
+      ignore_empty_input = true;
+      text_trim = true;
       hide_cursor = true;
       no_fade_in = true;
     };
@@ -16,18 +23,27 @@
       blur_size = 7;
     }];
 
+    # Information
+    label = [{
+      text = ''cmd[update:1000] echo -e "$(~/.config/hyprlock/info.sh)"'';
+      color = "#CECDC3";
+      font_size = 12;
+      position = "-20, -510";
+      halign = "right";
+      valign = "center";
+    }];
+
     input-field = [{
       size = "200, 50";
       position = "0, -80";
       monitor = "";
       dots_center = true;
       fade_on_empty = false;
-      font_color = "rgb(202, 211, 245)";
-      inner_color = "rgb(91, 96, 120)";
-      outer_color = "rgb(24, 25, 38)";
+      font_color = "#1C1B1A";
+      inner_color = "#CECDC3";
+      outer_color = "#1C1B1A";
       outline_thickness = 5;
-      placeholder_text =
-        '''/'<span foreground="##cad3f5">Password...</span>'/'';
+      placeholder_text = ''<span foreground="##CECDC3">Password...</span>'';
       shadow_passes = 10;
     }];
   };
