6c949285e5c2371bd751bbcd5b525a427f999c67
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-09-23 09:27:57 -0500

    vscodium

--- os/apps.nix
+++ os/apps.nix
@@ -40,8 +40,19 @@
     figma-linux
     xdg-utils
     nwg-displays
-    vscode-fhs
-    vscodium
+    (vscode-with-extensions.override {
+      vscode = vscodium;
+      vscodeExtensions = with vscode-extensions; [
+        vscode-extensions.jnoortheen.nix-ide
+      ] #++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+  #      {
+  #        name = "remote-ssh-edit";
+  #        publisher = "ms-vscode-remote";
+  #        version = "0.47.2";
+  #        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+  #      }
+  #    ];
+    })
     inputs.zen-browser.packages."${system}".default
 
     # neovim          # Text editor
