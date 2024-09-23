a1b88c847e1662e31fd898fb36ed6e2477a96bd5
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-09-23 09:28:46 -0500

    vscodium

--- os/apps.nix
+++ os/apps.nix
@@ -42,16 +42,17 @@
     nwg-displays
     (vscode-with-extensions.override {
       vscode = vscodium;
-      vscodeExtensions = with vscode-extensions; [
-        vscode-extensions.jnoortheen.nix-ide
-      ] #++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-  #      {
-  #        name = "remote-ssh-edit";
-  #        publisher = "ms-vscode-remote";
-  #        version = "0.47.2";
-  #        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-  #      }
-  #    ];
+      vscodeExtensions = with vscode-extensions;
+        [
+          vscode-extensions.jnoortheen.nix-ide
+        ]; # ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      #      {
+      #        name = "remote-ssh-edit";
+      #        publisher = "ms-vscode-remote";
+      #        version = "0.47.2";
+      #        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+      #      }
+      #    ];
     })
     inputs.zen-browser.packages."${system}".default
 
