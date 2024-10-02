b6e7d7dba7733e011063218adb283477dd1eda80
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-10-02 14:34:42 -0500

    vpn

--- os/modules/vpn.nix
+++ os/modules/vpn.nix
@@ -2,5 +2,6 @@
   programs.openvpn3.enable = true;
   services.openvpn.servers = {
     HTBThe4s = { config = ''config /home/logan/cyber/lab_the4s.ovpn''; };
+    HTBAcademy = { config = ''config /home/logan/cyber/academy-regular.ovpn''; };
   };
 }
