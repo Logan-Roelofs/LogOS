20a832bf867be87aae9cc4a090f23ee069a59b1b
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-10-02 14:50:45 -0500

    wireguard-ui

--- os/modules/vpn.nix
+++ os/modules/vpn.nix
@@ -1,5 +1,11 @@
 { config, pkgs, ... }: {
+  
+  environment.systemPackages = [
+    pkgs.wireguard-ui
+  ];
+  
   programs.openvpn3.enable = true;
+
   services.openvpn.servers = {
     HTBThe4s = { config = ''config /home/logan/cyber/lab_the4s.ovpn''; };
     HTBAcademy = { config = ''config /home/logan/cyber/academy-regular.ovpn''; };
