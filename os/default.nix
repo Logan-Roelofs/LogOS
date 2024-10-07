f92937a2f76e879adbb9a16293b30800911ca71c
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-10-07 10:59:44 -0500

    gnome

--- os/default.nix
+++ os/default.nix
@@ -10,6 +10,7 @@
     ./home-manger.nix
     ./stylix.nix
     ./modules/gnome.nix
+    #./modules/hyprland.nix
   ];
 
   networking.extraHosts = ''
@@ -109,22 +110,6 @@
   };
 
   networking.firewall.enable = false;
-
-  # Hyprland Configuration
-
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  environment.variables.KITTY_DISABLE_WAYLAND = "1";
-  # Define Common System Fonts
-  fonts.packages = with pkgs; [
-    font-awesome
-    ubuntu_font_family
-    nerdfonts
-    monaspace
-  ];
 
   # Remove nixos documentation
   documentation.enable = false;
---
+++ os/modules/hyprland.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, lib, inputs, username, ... }: {
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.variables.KITTY_DISABLE_WAYLAND = "1";
+  # Define Common System Fonts
+  fonts.packages = with pkgs; [
+    font-awesome
+    ubuntu_font_family
+    nerdfonts
+    monaspace
+  ];
+
+}
