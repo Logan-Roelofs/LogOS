799f1f278b1b4e19e0c3c188e535e606f61b29f1
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-10-07 11:07:37 -0500

    gnome

--- os/modules/gnome.nix
+++ os/modules/gnome.nix
@@ -1,3 +1,25 @@
 { config, pkgs, inputs, ... }: {
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  }
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+    gnome-terminal
+    gedit # text editor
+    epiphany # web browser
+    geary # email reader
+    evince # document viewer
+    gnome-characters
+    totem # video player
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+  ]);
 }
