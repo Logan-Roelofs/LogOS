1be4eb31d904ed19b84cf9696531250724fca5d0
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-10-07 11:37:02 -0500

    gnome

--- os/modules/gnome.nix
+++ os/modules/gnome.nix
@@ -4,42 +4,43 @@
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
-  environment.gnome.excludePackages = (with pkgs;
-    [
-      gnomeExtensions.gsnap # gnome tile windows
-    ]) ++ (with pkgs.gnome; [
-      cheese # webcam tool
-      gnome-music
-      gnome-terminal
-      epiphany # web browser
-      geary # email reader
-      evince # document viewer
-      gnome-characters
-      totem # video player
-      tali # poker game
-      iagno # go game
-      hitori # sudoku game
-      atomix # puzzle game
-      baobab
-      epiphany
-      pkgs.gnome-text-editor
-      gnome-calculator
-      gnome-calendar
-      gnome-characters
-      gnome-clocks
-      pkgs.gnome-console
-      gnome-contacts
-      gnome-font-viewer
-      gnome-logs
-      gnome-maps
-      gnome-music
-      gnome-system-monitor
-      gnome-weather
-      pkgs.loupe
-      nautilus
-      pkgs.gnome-connections
-      simple-scan
-      totem
-      yelp
-    ]);
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.gsnap
+  ];
+  environment.gnome.excludePackages = (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+    gnome-terminal
+    epiphany # web browser
+    geary # email reader
+    evince # document viewer
+    gnome-characters
+    totem # video player
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+    baobab
+    epiphany
+    pkgs.gnome-text-editor
+    gnome-calculator
+    gnome-calendar
+    gnome-characters
+    gnome-clocks
+    pkgs.gnome-console
+    gnome-contacts
+    gnome-font-viewer
+    gnome-logs
+    gnome-maps
+    gnome-music
+    gnome-system-monitor
+    gnome-weather
+    pkgs.loupe
+    nautilus
+    pkgs.gnome-connections
+    simple-scan
+    totem
+    yelp
+  ]);
+
 }
