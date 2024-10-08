95bcada89b2c45ceca864ea177c83269ebded2de
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-10-08 12:35:49 -0500

    ags edit

--- dotfiles/ags/service/logs.ts
+++ dotfiles/ags/service/logs.ts
@@ -1,12 +1,8 @@
-import icons from "lib/icons"
-import { bash, dependencies } from "lib/utils"
+import { bash } from "lib/utils"
 
 App.addIcons(`${App.configDir}/assets`)
 
-const COLORS_CACHE = Utils.CACHE_DIR + "/colorpicker.json"
-const MAX_NUM_COLORS = 10
-
-class ColorPicker extends Service {
+class Logs extends Service {
     static {
         Service.register(this, {}, {
             "colors": ["jsobject"],
@@ -14,38 +10,12 @@ class ColorPicker extends Service {
     }
 
     #notifID = 0
-    #colors = JSON.parse(Utils.readFile(COLORS_CACHE) || "[]") as string[]
 
-    get colors() { return [...this.#colors] }
-    set colors(colors) {
-        this.#colors = colors
-        this.changed("colors")
-    }
-
-    // TODO: doesn't work?
-    async wlCopy(color: string) {
-        if (dependencies("wl-copy"))
-            bash(`wl-copy ${color}`)
-    }
 
     readonly pick = async () => {
-        if (!dependencies("hyprpicker"))
-            return
-
         const color = await bash("/home/logan/.config/rofi/launchers/type-3/launcher.sh")
         if (!color)
             return
-
-        this.wlCopy(color)
-        const list = this.colors
-        if (!list.includes(color)) {
-            list.push(color)
-            if (list.length > MAX_NUM_COLORS)
-                list.shift()
-
-            this.colors = list
-            Utils.writeFile(JSON.stringify(list, null, 2), COLORS_CACHE)
-        }
 
         this.#notifID = await Utils.notify({
             id: this.#notifID,
@@ -55,4 +25,4 @@ class ColorPicker extends Service {
     }
 }
 
-export default new ColorPicker
+export default new Logs
