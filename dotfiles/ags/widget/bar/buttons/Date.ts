a69e88931963f89eb3f2d45d994a4b361ac9aea4
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-10-08 13:04:27 -0500

    ags edit

--- dotfiles/ags/widget/bar/buttons/Date.ts
+++ dotfiles/ags/widget/bar/buttons/Date.ts
@@ -1,11 +1,6 @@
 import { clock } from "lib/variables";
 import PanelButton from "../PanelButton";
 import options from "options";
-import GLib from "gi://GLib?version=2.0"
-
-const dateTime = Variable(GLib.DateTime.new_now_local(), {
-  poll: [1000, () => GLib.DateTime.new_now_local()],
-});
 
 const { format, action } = options.bar.date;
 const time = Utils.derive([clock, format], (c, f) => c.format(f) || "");
@@ -15,16 +10,13 @@ export default () =>
     window: "datemenu",
     on_clicked: action.bind(),
     vertical: true,
-    children: [
-      Widget.Label({
-        className: "date",
-        justification: "center",
-        label: dateTime.bind().as((date) => date.format("%a %d")),
-      }),
-      Widget.Label({
-        className: "time",
-        justification: "center",
-        label: dateTime.bind().as((time) => time.format("%I:%M")),
-      }),
-    ],
+    child: Widget.Box({
+      children: [
+        Widget.Label({
+          className: "date",
+          justification: "center",
+          label: time.bind(),
+        }),
+      ],
+    }),
   });
