2da424a92723fbbf45aa9f86130e67a660c562cf
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-10-09 12:16:51 -0500

    ags edit

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -5,7 +5,6 @@
       "${config.home.homeDirectory}/.config/logos/dotfiles/wp/";
     recursive = true;
   };
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
