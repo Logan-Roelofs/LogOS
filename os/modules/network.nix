6c0defb2ab08a0018dfb1ae027ef541e66c93ccc
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-10-09 13:31:34 -0500

    ags edit

--- os/modules/network.nix
+++ os/modules/network.nix
@@ -12,11 +12,4 @@
     gtk3
     gnome.gnome-control-center
   ]; 
-  gnome-control-center = pkgs.gnome.gnome-control-center.runCommand "${pkgs.gnome.gnome-control-center.name}" { } ''
-      cp -R ${pkgs.gnome.gnome-control-center} $out
-      chmod -R +w $out
-      rm $out/share/applications/gnome-{online-accounts,sharing}-panel.desktop
-      find $out -type f -exec sed -i -e "s|${pkgs.gnome.gnome-control-center}|$out|g" {} \;
-    '';
-
 }
