14a5df44234c2fdfc216801dc96cfac57516d2fd
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-10-16 14:13:06 -0500

    ags edit

--- home/modules/gtk.nix
+++ home/modules/gtk.nix
@@ -5,6 +5,6 @@
   gtk.theme.name = "Adwaita-dark";
   gtk.cursorTheme.package = pkgs.bibata-cursor;
   gtk.cursorTheme.name = "Bibata-Modern-Ice";
-  gtk.iconTheme.package = gruvboxPlus;
+  gtk.iconTheme.package = pkgs.gruvboxPlus;
   gtk.iconTheme.name = "GruvboxPlus";
 }
