42b8e2642398979655a47e0f03abd3cb7f67b3ea
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-10-16 14:12:47 -0500

    ags edit

--- home/modules/gtk.nix
+++ home/modules/gtk.nix
@@ -1,6 +1,10 @@
 { inputs, config, pkgs, ... }: {
 
- gtk.enable = true;
- gtk.theme = "Adwaita";
- gtk.package = home.packages.pkgs.adementary-theme;
+  gtk.enable = true;
+  gtk.theme.package = pkgs.adwaita-gtk3-theme;
+  gtk.theme.name = "Adwaita-dark";
+  gtk.cursorTheme.package = pkgs.bibata-cursor;
+  gtk.cursorTheme.name = "Bibata-Modern-Ice";
+  gtk.iconTheme.package = gruvboxPlus;
+  gtk.iconTheme.name = "GruvboxPlus";
 }
