7e360e8193eac63fc19ecdb50497adb0fcfc5a3b
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-10-16 14:18:26 -0500

    ags edit

--- home/modules/gtk.nix
+++ home/modules/gtk.nix
@@ -1,7 +1,4 @@
 { inputs, config, pkgs, ... }: {
   gtk.enable = true;
-  gtk.theme.package = pkgs.theme-vertex;
-  gtk.theme.name = "theme-vertex";
-  gtk.cursorTheme.package = pkgs.bibata-cursors;
-  gtk.cursorTheme.name = "Bibata-Modern-Ice";
+  qt.enable = true;
 }
