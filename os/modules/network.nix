fb5ed65fb31ac052d5e34d70e6e941573acf4d91
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-10-16 10:55:41 -0500

    ags edit

--- os/modules/network.nix
+++ os/modules/network.nix
@@ -20,30 +20,8 @@
     gnome.gnome-control-center
     networkmanager-openvpn
     gnome.networkmanager-openvpn
-    networkmanager-fortisslvpn
-    gnome.networkmanager-fortisslvpn
-    networkmanager-openconnect
-    gnome.networkmanager-openconnect
-    networkmanager-vpnc
-    gnome.networkmanager-vpnc
-    networkmanager-sstp
-    networkmanager-l2tp
-    networkmanager-iodine
-    gnome.networkmanager-iodine
-    networkmanager_strongswan
   ];
   networking.wg-quick.interfaces = {
-    wg-proton = {
-      address = [ "10.2.0.2/32" ];
-      dns = [ "10.2.0.1" ]; # mullvad public dns
-      privateKeyFile = "../../dotfiles/vpn/wg-proton-privatekey";
-      peers = [
-        {
-          publicKey = "Lblqk058zWqS+I7pxpKo4ffxfceki0KTTGdEdvI4zUg=";
-          allowedIPs = [ "0.0.0.0/0" ]; # Only send communication through mullvad if it is in the range of the given ips, allows for split tunneling
-          endpoint = "149.22.88.59:51820"; # my selected mullvad enpoint
-        }
-      ];
-    };
+    wg-proton.configFile = "/home/logan/.config/logos/dotfiles/vpn/wg-proton-US-FREE-285032.conf" 
   };
 }
