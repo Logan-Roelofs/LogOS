19cf913fecfcaa2f7460975542c80606cb71e3f6
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-11-03 12:34:50 -0600

    formatting

--- home/modules/desktop.nix
+++ home/modules/desktop.nix
@@ -1,9 +1,9 @@
 { inputs, config, pkgs, ... }: {
   xdg.desktopEntries = {
-    control-pannel = {
-      name = "control-pannel";
-      genericName = "Control Pannel";
-      exec = "firefox %U";
+    control-center = {
+      name = "control-center";
+      genericName = "Control Center";
+      exec = "env XDG_CURRENT_DESKTOP=GNOME gnome-control-center %U";
       terminal = false;
       categories = [ "Application" ];
     };
