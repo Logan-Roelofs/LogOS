ac0d96943323a52fe5de44bd5546af02c6178f85
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-11-03 12:11:03 -0600

    add desktop file for control pannel

--- os/modules/control-pannel.nix
+++ os/modules/control-pannel.nix
@@ -1,11 +1,14 @@
 { config, pkgs, inputs, ... }: {
-  environment.etc."/etc/profiles/per-user/logan/share/applications/control-pannel.desktop".source = ''
-  [Desktop Entry]
-  Actions=new-empty-window
-  Exec=codium %F
-  Icon=vscodium
-  Name=HELLO
-  Type=Application
-  Version=1
-  '';
+  environment.etc."/etc/profiles/per-user/logan/share/applications/control-pannel.desktop" = {
+    mode = "0644";
+    text = ''
+      [Desktop Entry]
+      Actions=new-empty-window
+      Exec=codium %F
+      Icon=vscodium
+      Name=HELLO
+      Type=Application
+      Version=1
+    '';
+  };
 }
