b9ba5aee78a42c43f94261455d00d61102603a6b
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-11-03 11:35:11 -0600

    rm homemanger declaration of rofi config

--- home/modules/rofi.nix
+++ home/modules/rofi.nix
@@ -1,8 +1,7 @@
 { inputs, config, pkgs, ... }: {
-  programs.rofi.enable = true;
-    home.file.".config/rofi" = {
+  home.file.".config/rofi" = {
     source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/.config/logos/dotfiles/rofi/";
+    "${config.home.homeDirectory}/.config/logos/dotfiles/rofi/";
     recursive = true;
   };
 }
