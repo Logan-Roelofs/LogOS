583b3f561617b54ec64f22342daaa283dc386c98
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-11-07 11:34:21 -0500

    add wsl

--- flake.nix
+++ flake.nix
@@ -12,9 +12,13 @@
     };
     nixpkgs-python.url = "github:cachix/nixpkgs-python";
     zen-browser.url = "github:MarceColl/zen-browser-flake";
+    NixOS-WSL = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, NixOS-WSL, ... }@inputs:
     let
       username = "logan";
       system = "x86_64-linux";
@@ -37,5 +41,20 @@
           }
         ];
       };
+      nixosConfigurations.winLogos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs username home-manager; };
+        modules = [
+          { nix.registry.nixpkgs.flake = nixpkgs; }
+          ./os/wsl.nix
+          NixOS-WSL.nixosModules.wsl
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.logan = import ./home/default.nix;
+            home-manager.extraSpecialArgs = { inherit inputs username; };
+          }
+        ];
+      }
     };
 }
---
+++ os/wsl.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, inputs, username, home-manager, ... }: {
+ wsl.useWindowsDriver = true;
+  programs.sway.enable = true;
+  wsl.enable = true;
+  wsl.defaultUser = "nixos";
+  wsl.startMenuLaunchers = true;
+  environment.variables = {
+    DONT_PROMPT_WSL_INSTALL = "true";
+  };
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = [
+    pkgs.vscodium
+    pkgs.emacs
+    pkgs.spotify
+    pkgs.firefox-unwrapped
+    pkgs.xdg-utils
+    pkgs.git
+  ];
+  system.stateVersion = "24.05"; # Do not change 
+}
