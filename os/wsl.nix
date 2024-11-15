509f9dc354b8a12ae99a3284d40c0c25b440d822
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-11-15 11:34:04 -0500

    add sudo

--- os/wsl.nix
+++ os/wsl.nix
@@ -11,6 +11,8 @@
   };
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
+  environment.shells = with pkgs; [ zsh ];
+  security.sudo.enable = true;
   nixpkgs.config.allowUnfree = true;
   services.xrdp.defaultWindowManager = "xterm";
   environment.systemPackages = [
@@ -23,6 +25,7 @@
     pkgs.gtk2-x11
     pkgs.gtk2
     pkgs.xfce.xfce4-session
+    pkgs.sudo
   ];
   system.stateVersion = "24.05"; # Do not change 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
