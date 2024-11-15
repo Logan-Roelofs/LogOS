497218e5dcea3b4c7af4000e80ffd0ce2ee868fd
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-11-15 11:45:47 -0500

    rm wsl gui that did not work

--- README.md
+++ README.md
@@ -6,4 +6,3 @@ This is my personal NixOS configration that I like to call "LogOS" enjoy!
 
 ## WSL 
 
-1. In WSL to have better GUI app interaction install VCXSRV[https://sourceforge.net/projects/vcxsrv/files/latest/download] on windows and set it up according to the WIKI 
--- os/wsl.nix
+++ os/wsl.nix
@@ -6,28 +6,21 @@
   wsl.startMenuLaunchers = true;
   environment.variables = {
     DONT_PROMPT_WSL_INSTALL = "true";
-    LIBGL_ALWAYS_INDIRECT = "1";
-    DISPLAY = "$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0";
   };
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ];
   programs.zsh.shellAliases = {
-  sudo = "/run/wrappers/bin/sudo";
-};
+    sudo = "/run/wrappers/bin/sudo";
+  };
   nixpkgs.config.allowUnfree = true;
   services.xrdp.defaultWindowManager = "xterm";
   environment.systemPackages = [
     pkgs.vscodium
     pkgs.emacs
-    pkgs.spotify
     pkgs.firefox-unwrapped
     pkgs.xdg-utils
     pkgs.git
-    pkgs.gtk2-x11
-    pkgs.gtk2
-    pkgs.xfce.xfce4-session
-    pkgs.xfce.xfce4-settings
   ];
   system.stateVersion = "24.05"; # Do not change 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
