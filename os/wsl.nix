d22707a9c5f2d567e809568c1db8a96abe7bf62b
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-11-15 11:42:36 -0500

    add xfce.xfce4-settings

--- os/wsl.nix
+++ os/wsl.nix
@@ -7,7 +7,7 @@
   environment.variables = {
     DONT_PROMPT_WSL_INSTALL = "true";
     LIBGL_ALWAYS_INDIRECT = "1";
-    DISPLAY= "$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0";
+    DISPLAY = "$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0";
   };
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
@@ -27,6 +27,7 @@
     pkgs.gtk2-x11
     pkgs.gtk2
     pkgs.xfce.xfce4-session
+    pkgs.xfce.xfce4-settings
   ];
   system.stateVersion = "24.05"; # Do not change 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
