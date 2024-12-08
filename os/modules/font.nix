7e916e9330d4e2843f7470eff16f692111388a9e
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2024-12-08 16:03:34 -0600

    bun

--- os/modules/font.nix
+++ os/modules/font.nix
@@ -1,10 +1,11 @@
 { config, pkgs, lib, inputs, username, ... }: {
-  fonts.packages = with pkgs; [
-    font-awesome
-    ubuntu_font_family
-    nerdfonts
-    monaspace
-  ];
+  #  fonts.packages = with pkgs; [
+  #    font-awesome
+  #ubuntu_font_family
+  #nerdfonts
+  #monaspace
+  #];
+  font.packages = [ pkgs.monaspace pkgs.font-awesome pkgs.ubuntu_font_family ... ] ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts)
   environment.systemPackages = with pkgs;
     [
       adwaita-icon-theme # Icon theme
