c2a60dc8359757c9781243b222a5d63cfb40cf3d
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2025-03-25 13:33:00 -0500

    Update iso.nix

--- os/iso.nix
+++ os/iso.nix
@@ -6,4 +6,14 @@
 
   nixpkgs.hostPlatform = "x86_64-linux";
 
+  nix = {
+    settings = {
+      substituters = [
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
+    };
+  };
 }
